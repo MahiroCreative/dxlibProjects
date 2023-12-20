418387845162e1f40b91c4775d3561aed75bd4b8
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2023-12-20 17:00:47 +0900

    first

---
+++ dxlibFirstGame/main.cpp
@@ -0,0 +1,50 @@
+#include "DxLib.h"
+#include "Game.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	/*Dxlib初期化*/
+	SetGraphMode(Game::ScreenSizeX, Game::ScreenSizeY, 32);//画面サイズと解像度
+	ChangeWindowMode(true);//Windowモード
+	if (DxLib_Init() == -1) { return -1;}//Dxlib初期化
+	SetDrawScreen(DX_SCREEN_BACK);//ダブルバッファリング
+
+	/*変数*/
+	LONGLONG roopStartTime;
+
+
+	/*ゲームループ部*/
+	while(true)
+	{
+		//ループ開始時刻の確保
+		roopStartTime = GetNowHiPerformanceCount();
+
+		//裏画面の初期化
+		ClearDrawScreen();
+
+		/*ゲーム処理部*/
+
+
+
+
+		//裏画面を表へ
+		ScreenFlip();
+
+		//リフレッシュ処理(-1ならエラー)
+		if (ProcessMessage() < 0) { break; }
+
+		//ループ終了処理
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) { break; }
+
+		//fps固定(60fps:16.66ms)
+		//ループ開始時刻から16.66ms経つまで停止
+		while (GetNowHiPerformanceCount() - roopStartTime < 16667) {}
+
+	}
+
+
+	/*終了処理*/
+	DxLib_End();//Dxlib終了処理
+	return 0;//終了 
+}
