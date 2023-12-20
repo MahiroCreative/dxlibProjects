2cd6e209f7229722b4720eed67d0a7252b749bc1
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2023-12-20 12:03:11 +0900

    u

--- dxlibFurukawaProject2/main.cpp
+++ dxlibFurukawaProject2/main.cpp
@@ -7,9 +7,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 {
 	// 一部の関数はDxLib_Init()の前に実行する必要がある
 	ChangeWindowMode(true);
-	// 画面サイズの変更
-		
-
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
