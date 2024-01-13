dbab3650285f713d93f5f34062ba21443cad49b6
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2024-01-13 20:57:30 +0900

    up

---
+++ dxlibFirstGame2/TitleScene.h
@@ -0,0 +1,36 @@
+#pragma once
+
+class TitleScene
+{
+private:
+	int ans;
+public:
+	TitleScene() :
+		ans(0)
+	{
+	};
+	/*ƒƒ“ƒoŠÖ”*/
+	int Update()
+	{
+		//debug.
+		if (MyKeyInput::isDownKey(KEY_INPUT_RETURN))
+		{
+			ans += 1;
+		}
+
+		return 0;
+	}
+	void Draw()
+	{
+		if (ans > 1)
+		{
+			DrawString(600, 480, "title2", GetColor(255, 255, 255));
+		}
+		else
+		{
+			DrawString(600, 480, "title", GetColor(255, 255, 255));
+		}
+
+	}
+
+};
