6cb0b4cda1bb75159920baa33a904af292b3fe89
GoToSchoolTeams <schoolgoto@gmail.com>
2024-01-16 08:38:12 +0900

    del

--- dxlibFirstGame1/GameManager.cpp
+++ dxlibFirstGame1/GameManager.cpp
@@ -6,7 +6,7 @@
 #include "RankingScene.h"
 
 //Dxlibのエントリーポイント
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	/*定数*/
 	//画面サイズ
