a4f01e1d1b122fa9093f218b02ec122139897ed8
GoToSchoolTeams <schoolgoto@gmail.com>
2024-01-24 15:47:39 +0900

    up

--- dxlibStudySceneManager1/main.cpp
+++ dxlibStudySceneManager1/main.cpp
@@ -1,15 +1,22 @@
 #include "DxLib.h"
 #include <math.h>
 
-using namespace DxLib;
-
 /*概要*/
-//C言語の『関数まで』で組んだシーン管理(シーンマネージャー)
-//分からない学生はCの『基礎文法』を復習してください。
+//名称:DxlibGameTemplate1
+//C言語の『関数まで』で組んだテンプレートです。
+// 非常に簡易的ですが
+// ・シーン遷移(グローバル変数とif文で実装)
+// ・インプットシステム(グローバル変数の乱用により実装)
+// ・円の当たり判定(ベクトルを使わずに実装)
+// などを備えています。
+// 作成するゲームの下地として使うよりは『読んで理解できるか？』で学習進度を確認してください。
+// 教科書やDxlibのリファレンスを見ながらの理解で構いません。
+// 分からない学生はCの学習をやり直してください。
 
 /*構造*/
 //それぞれのシーンを関数(メソッド)として分割して、
 //実行するメソッドを切り替える事でシーン管理をしている。
+//シーンからの返り値で次にいくシーンを判断しています。
 
 /*注意*/
 //fpsの差による挙動の違いは考慮していません。
@@ -109,13 +116,13 @@ int TitleScene()
 	bool gameRoop = true;
 	int nextScene = TITLESCENE;
 	int arrowPosY = 440;
+	int countFrame = 0;
 
 	/*ゲーム処理*/
 	while (gameRoop)
 	{
-
 		/*計算処理*/
-		//Down.
+		//Input Down.
 		if (InputDown())
 		{
 			if (arrowPosY == 440)
@@ -127,7 +134,7 @@ int TitleScene()
 				arrowPosY = 440;
 			}
 		}
-		//Up.
+		//Input Up.
 		if (InputUp())
 		{
 			if (arrowPosY == 440)
@@ -139,26 +146,35 @@ int TitleScene()
 				arrowPosY = 440;
 			}
 		}
-			
+
+
+		/*タイマ更新*/
+		countFrame++;
+		if (countFrame > 80000) { countFrame = 0; }//一定以上数が増えたら初期化(数は適当)
+
 		/*Draw処理*/
 		//裏画面の初期化
 		ClearDrawScreen();
 
 		//タイトルロゴ
-		SetFontSize(80);//フォントサイズ上昇
-		DrawString(460, 240, "DxlibGame", GetColor(255, 255, 255));
+		SetFontSize(80);//フォントサイズ変更
+		DrawString(440, 240, "DxlibGame", GetColor(255, 255, 255));
+		SetFontSize(40);//フォントサイズ上
+		DrawString(460, 320, "-GameTemplate1-", GetColor(255, 255, 255));
 		SetFontSize(20);//フォントサイズ初期化
-
 		//ゲームシーンテキスト
 		DrawString(600, 440, "START", GetColor(255, 255, 255));
-
 		//ゲームエンドテキスト
 		DrawString(600, 480, "END", GetColor(255, 255, 255));
+		//矢印表示(点滅させる)
+		if ((countFrame % 60) < 32)
+		{
+			DrawString(560, arrowPosY, "->", GetColor(255, 255, 255));
+		}
 
-		//矢印
-		DrawString(560, arrowPosY, "->", GetColor(255, 255, 255));
 
-		//DebugDraw処理
+
+		/*DebugDraw処理*/
 		DrawString(0, 0, "TitleScene", GetColor(255, 255, 255));//シーン名表示
 
 		//裏画面を表へ
@@ -168,7 +184,7 @@ int TitleScene()
 		//エンターでシーン変更
 		if (InputEnter())
 		{
-			if (arrowPosY ==440)
+			if (arrowPosY == 440)
 			{
 				return GAMESCENE;
 			}
@@ -190,6 +206,7 @@ int GameScene()
 	/*変数*/
 	bool gameRoop = true;
 	bool isShot = false;
+	bool isEnemyShot = false;
 	bool isHitEnemy = false;
 	bool isHitPlayer = false;
 	int nextScene = GAMESCENE;
@@ -203,25 +220,30 @@ int GameScene()
 	int bulletPosX = playerPosX;
 	int bulletPosY = playerPosY;
 	int bulletR = 8;
+	int enemyBulletPosX = enemyPosX;
+	int enemyBulletPosY = enemyPosY;
+	int enemyBulletR = 64;
 	int playerSpeed = 3;
 	int bulletSpeed = 8;
+	int enemyBulletSpeed = 3;
+	int enemySpeed = 1;
 	int score = 0;
 
 	/*ゲーム処理*/
 	while (gameRoop)
 	{
-
-		/*計算処理*/
-		//player操作
+		/*player動作*/
 		playerPosY -= CheckHitKey(KEY_INPUT_W) * playerSpeed;//up
 		playerPosY += CheckHitKey(KEY_INPUT_S) * playerSpeed;//down
 		playerPosX += CheckHitKey(KEY_INPUT_D) * playerSpeed;//right.
 		playerPosX -= CheckHitKey(KEY_INPUT_A) * playerSpeed;//left.
-		//shot操作
+		//bullet発射
 		if (isShot == false)
 		{
 			if (GetMouseInput() == MOUSE_INPUT_LEFT) isShot = true;
 		}
+
+		/*playerBulletの動作*/
 		//bullet位置更新
 		if (isShot)
 		{
@@ -232,18 +254,58 @@ int GameScene()
 			bulletPosX = playerPosX;
 			bulletPosY = playerPosY;
 		}
-		//bullet位置初期化
-		if(bulletPosX>1280)
+		//画面端でbullet位置初期化
+		if (bulletPosX > 1280)
 		{
 			isShot = false;
 			bulletPosX = playerPosX;
 			bulletPosY = playerPosY;
 		}
 
-		//エネミーの当たり判定
+		/*エネミー動作*/
+		//エネミー位置更新
+		enemyPosY += enemySpeed;
+		//反転
+		if (enemyPosY > 560)
+		{
+			enemySpeed = -enemySpeed;
+		}
+		else if (enemyPosY < 160)
+		{
+			enemySpeed = -enemySpeed;
+		}
+		//enemyBullet発射
+		if (isEnemyShot == false)
+		{
+			if (enemyPosY % 3 == 0) isEnemyShot = true;
+		}
+
+		/*enemyBullet*/
+		//enemyBulletの動き
+		if (isEnemyShot)
+		{
+			enemyBulletPosX = enemyBulletPosX - enemyBulletSpeed;
+		}
+		else
+		{
+			enemyBulletPosX = enemyPosX;
+			enemyBulletPosY = enemyPosY;
+		}
+		//enemyBullet位置初期化
+		if (enemyBulletPosX < -enemyBulletR*2)
+		{
+			isEnemyShot = false;
+			enemyBulletPosX = enemyPosX;
+			enemyBulletPosY = enemyPosY;
+			//enemyBulletSpeedの更新
+			enemyBulletSpeed++;
+		}
+
+		/*当たり判定*/
+		//enemy
 		int delX = bulletPosX - enemyPosX;
 		int delY = bulletPosY - enemyPosY;
-		int Length = (int)sqrt(delX*delX + delY*delY);//エネミーと弾の距離
+		int Length = (int)sqrt(delX * delX + delY * delY);//エネミーと弾の距離
 		if (bulletR + enemyColR > Length)
 		{
 			isHitEnemy = true;
@@ -252,28 +314,27 @@ int GameScene()
 		{
 			isHitEnemy = false;
 		}
-		//プレイヤーの当たり判定
+		//player
+		delX = enemyBulletPosX - playerPosX;
+		delY = enemyBulletPosY - playerPosY;
+		Length = (int)sqrt(delX * delX + delY * delY);//プレイヤーと弾の距離
+		if (enemyBulletR + playerColR > Length)
+		{
+			isHitPlayer = true;
+		}
 
-
-		//スコアの更新
+		/*スコアの更新*/
 		if (isHitEnemy)
 		{
 			score++;
 		}
-
-		//ハイスコアの更新
-		if (score < MaxScore)
-		{
-			MaxScore = score;
-		}
-
 
 		/*Draw処理*/
 		//裏画面の初期化
 		ClearDrawScreen();
 
 		//player
-		DrawRotaGraph(playerPosX,playerPosY,1,0,playerHandle,true);//指定した位置に画像の中心が来る
+		DrawRotaGraph(playerPosX, playerPosY, 1, 0, playerHandle, true);//指定した位置に画像の中心が来る
 		//bullet.
 		if (isShot)
 		{
@@ -288,13 +349,21 @@ int GameScene()
 		{
 			DrawCircle(enemyPosX, enemyPosY, 240, GetColor(0, 0, 255), 1);
 		}
+		//enemyBullet
+		if (isEnemyShot)
+		{
+			DrawCircle(enemyBulletPosX, enemyBulletPosY, enemyBulletR, GetColor(255, 0, 0), 1);
+		}
 		//score
 		DrawFormatString(10, 700, GetColor(0, 255, 255), "Score:%d", score);
+		//MaxScore
+		DrawFormatString(1100, 0, GetColor(255, 255, 255), "MaxScore:%d", MaxScore);
 
-		//DebugDraw処理
-		DrawString(0, 0, "GameScene:WASDで操作", GetColor(255, 255, 255));//シーン名表示
-		DrawCircle(playerPosX,playerPosY,playerColR,GetColor(0,255,0),0);//PlayerCol
+		/*DebugDraw処理*/
+		DrawString(0, 0, "GameScene：WASDで操作・Enterでタイトル", GetColor(255, 255, 255));//シーン名表示
+		DrawCircle(playerPosX, playerPosY, playerColR, GetColor(0, 255, 0), 0);//PlayerCol
 		DrawCircle(enemyPosX, enemyPosY, enemyColR, GetColor(0, 255, 0), 0);//enemyCol
+		DrawCircle(enemyBulletPosX, enemyBulletPosY, enemyBulletR, GetColor(0, 255, 0), 0);//enemyCol
 
 		//裏画面を表へ
 		ScreenFlip();
@@ -303,6 +372,15 @@ int GameScene()
 		//エンターでシーン変更
 		if (InputEnter())
 		{
+			return TITLESCENE;
+		}
+		if(isHitPlayer)
+		{
+			//スコアの更新
+			if (score> MaxScore)
+			{
+				MaxScore = score;
+			}
 			return TITLESCENE;
 		}
 
@@ -323,7 +401,7 @@ bool InputEnter()
 		isInputEnterHold = true;
 		return true;
 	}
-	else if(!CheckHitKey(KEY_INPUT_RETURN))
+	else if (!CheckHitKey(KEY_INPUT_RETURN))
 	{
 		isInputEnterHold = false;
 	}
