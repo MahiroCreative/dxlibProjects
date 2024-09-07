34d121ee6e9accd27aa3fe66feb185d63f8ac56a
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2024-09-08 02:06:41 +0900

    up

---
+++ dxlib3DGame/GameTool.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include "DxLib.h"
+#include "string"
+
+using namespace std;
+
+struct DebugString
+{
+	string value1;
+	string value2;
+	string value3;
+	string value4;
+	string value5;
+	string value6;
+	string value7;
+	string value8;
+	string value9;
+	string value10;
+	string value11;
+	string value12;
+};
+
+void DebugDraw(DebugString* temp)
+{
+	//白色
+	unsigned int Cr = GetColor(255, 255, 255);
+	//sting型をcharに変換
+	auto value1 = temp->value1.c_str();
+	auto value2 = temp->value2.c_str();
+	auto value3 = temp->value3.c_str();
+	auto value4 = temp->value4.c_str();
+	auto value5 = temp->value5.c_str();
+	auto value6 = temp->value6.c_str();
+	auto value7 = temp->value7.c_str();
+	auto value8 = temp->value8.c_str();
+	auto value9 = temp->value9.c_str();
+	auto value10 = temp->value10.c_str();
+	auto value11 = temp->value11.c_str();
+	auto value12 = temp->value12.c_str();
+	//表示
+	DrawFormatString(0, 0, Cr, value1);
+	DrawFormatString(0, 20, Cr, value2);
+	DrawFormatString(0, 40, Cr, value3);
+	DrawFormatString(0, 60, Cr, value4);
+	DrawFormatString(0, 80, Cr, value5);
+	DrawFormatString(0, 100, Cr, value6);
+	DrawFormatString(0, 120, Cr, value7);
+	DrawFormatString(0, 140, Cr, value8);
+	DrawFormatString(0, 160, Cr, value9);
+	DrawFormatString(0, 180, Cr, value10);
+	DrawFormatString(0, 200, Cr, value11);
+	DrawFormatString(0, 220, Cr, value12);
+}
--- dxlib3DGame/main.cpp
+++ dxlib3DGame/main.cpp
@@ -1,12 +1,13 @@
 ﻿#include "DxLib.h"
 #include "string"
+#include "GameMath.h"
+#include "GameTool.h"
 
 using namespace std;
 
 /*概要*/
 //ここでは画面サイズとゲームループのみ行う。
 //ゲームそのものの処理はGameMain以下で行う。
-
 
 //Dxlibのエントリーポイント
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
@@ -19,6 +20,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 	/*変数*/
 	LONGLONG roopStartTime = 0;
 	bool gameRoop = true;
+	DebugString dString;
 
 	/*Dxlib初期化*/
 	SetGraphMode(ScreenSizeX, ScreenSizeY, 32);//画面サイズと解像度
@@ -36,10 +38,35 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, 
 		ClearDrawScreen();
 
 		/*ゲーム処理*/
+		Vector3 v1;
+		Vector3 v2;
+		Vector3 v3;
+		Vector3 v4;
+		Vector3 v5;
+		Vector3 v6;
+		v1.X = 1.2f; v1.Y = 1.2f; v1.Z = 1.2f;
+		v2.X = 1.6f; v2.Y = 1.0f; v2.Z = 2.2f;
+		v3 = v1 + v2;
+		v4 = v1 - v2;
+		v5.X = 0.1f;
+		v5 += v1;
+		v6.Y = 0.2f;
+		v6 -= v2;
 
 		/*Debug表示*/
-		unsigned int Cr = GetColor(255,255,255);
-		DrawString(0,0,"test",Cr);
+		dString.value1 = to_string(v3.X);
+		dString.value2 = to_string(v3.Y);
+		dString.value3 = to_string(v3.Z);
+		dString.value4 = to_string(v4.X);
+		dString.value5 = to_string(v4.Y);
+		dString.value6 = to_string(v4.Z);
+		dString.value7 = to_string(v5.X);
+		dString.value8 = to_string(v5.Y);
+		dString.value9 = to_string(v5.Z);
+		dString.value10 = to_string(v6.X);
+		dString.value11 = to_string(v6.Y);
+		dString.value12 = to_string(v6.Z);
+		DebugDraw(&dString);
 
 
 		//裏画面を表へ
