a80554daba7cfb46d4f4ef0d6d7bed42693fc927
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2024-11-20 00:10:50 +0900

    f

---
+++ dxlib3DGameTemplate/MyDxlib/MyDxlib.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+#include "DxLib.h"
+#include "MyMath/MyMath.h"
+#include "MyTool/MyTool.h"
---
+++ dxlib3DGameTemplate/MyDxlib/MyMath/MyMath.h
@@ -0,0 +1,11 @@
+﻿//外部ライブラリに依存しない数学系
+//ライブラリをまとめたヘッダファイル
+#pragma once
+//C++標準ライブラリ
+#include <cassert>
+#include <string>
+#include <cmath>
+#include <numbers>
+//自作ライブラリ
+#include "Vector3.h"
+#include "Quaternion.h"
